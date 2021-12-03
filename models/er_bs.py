e6b4ad011b0cb272d4bea804cc5d21f6e09a029c
rbenaglia <riccardo.benaglia@energyway.it>
2021-12-03 11:42:03 +0100

    add batch shaping and experience replay with batch shaping

---
+++ models/er_bs.py
@@ -0,0 +1,102 @@
+# Copyright 2020-present, Pietro Buzzega, Matteo Boschini, Angelo Porrello, Davide Abati, Simone Calderara.
+# All rights reserved.
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+import os.path
+from torch.functional import F
+import torch
+from utils.buffer import Buffer
+from utils.args import *
+from models.utils.continual_model import ContinualModel
+from datasets import get_dataset
+from utils.batch_shaping import BatchShapingLoss
+import wandb
+
+
+def get_parser() -> ArgumentParser:
+    parser = ArgumentParser(description='Continual learning via'
+                                        ' Experience Replay with Shapiro.')
+    add_management_args(parser)
+    add_experiment_args(parser)
+    add_rehearsal_args(parser)
+    parser.add_argument('--bs_weight', type=float, required=True,
+                        help='Penalty weight.')
+    return parser
+
+
+class ErBs(ContinualModel):
+    NAME = 'er_bs'
+    COMPATIBILITY = [
+        'class-il',
+        # 'domain-il',
+        'task-il',
+        # 'general-continual'
+    ]
+
+    def __init__(self, backbone, loss, args, transform):
+        super(ErBs, self).__init__(backbone, loss, args, transform)
+        self.buffer = Buffer(self.args.buffer_size, self.device)
+        self.task = 0
+        self.classes = get_dataset(args).N_CLASSES_PER_TASK
+        self.n_task = get_dataset(args).N_TASKS
+        self.bs_loss = BatchShapingLoss()
+        if os.path.isfile('loss_task'):
+            os.remove('loss_task')
+        with open('loss_task', 'w') as obj:
+            obj.write(f'task, loss, shapiro_loss\n')
+
+    def observe(self, inputs, labels, not_aug_inputs):
+        real_batch_size = inputs.shape[0]
+
+        self.opt.zero_grad()
+        if not self.buffer.is_empty():
+            buf_inputs, buf_labels = self.buffer.get_data(
+                self.args.minibatch_size, transform=self.transform)
+            inputs = torch.cat((inputs, buf_inputs))
+            labels = torch.cat((labels, buf_labels))
+
+        outputs = self.net(inputs)
+        task_labels = labels // self.classes
+        eye = torch.eye(self.n_task).bool()
+        mask = torch.repeat_interleave(eye[task_labels], self.classes, 1)
+        masked_outputs = outputs[mask].reshape(inputs.shape[0], -1)
+        if self.task+1 < self.n_task:
+            masked_futures = outputs[:, (self.task+1)*self.classes: (self.task+2)*self.classes]
+            bs_loss = self.bs_loss(F.softmax(masked_futures, dim=1))
+        else:
+            bs_loss = 0
+
+        loss = self.loss(masked_outputs, labels % self.classes)
+        # wandb.log({'loss': loss, 'shap_loss': bs_loss})
+        # with open('loss_task', 'a') as obj:
+        #     obj.write(f'{self.task}, {loss}, {shap_loss}\n')
+        loss += bs_loss * self.args.bs_weight
+        loss.backward()
+        self.opt.step()
+
+        self.buffer.add_data(examples=not_aug_inputs,
+                             labels=labels[:real_batch_size])
+
+        return loss.item()
+
+    def end_task(self, dataset):
+        self.task += 1
+        # status = self.net.training
+        # self.net.eval()
+        # shapiri_test_total = torch.zeros(self.classes*self.n_task).to(self.device)
+        # for k, test_loader in enumerate(dataset.test_loaders):
+        #     shapiri_test = torch.zeros(self.classes * self.n_task).to(self.device)
+        #     for n, data in enumerate(test_loader):
+        #         inputs, labels = data
+        #         inputs, labels = inputs.to(self.device), labels.to(self.device)
+        #         if 'class-il' not in self.COMPATIBILITY:
+        #             outputs = self.net(inputs, k)
+        #         else:
+        #             outputs = self.net(inputs)
+        #         shapiri_test += self.shapiro_loss.shapiro_test(outputs)
+        #     shapiri_test /= n+1
+        #     shapiri_test_total += shapiri_test
+        # shapiri_test_total /= k+1
+        # # wandb.log({f"{num}": shapiri_test_total[num].item() for num in range(shapiri_test_total.shape[0])})
+        # # wandb.log({'logits': shapiri_test_total})
+        # self.net.train(status)
---
+++ utils/batch_shaping.py
@@ -0,0 +1,53 @@
+import torch
+from torch import nn
+
+
+class BatchShapingLoss(nn.Module):
+
+    def __init__(self, alpha: int = 0.6, beta: int = 0.4):
+        super(BatchShapingLoss, self).__init__()
+        self.distro = torch.distributions.beta.Beta(torch.Tensor([alpha]), torch.Tensor([beta]))
+        self.cache = {}
+
+    def forward(self, x: torch.Tensor, mask: torch.Tensor = None):
+        if mask is not None:
+            throw_cols = mask.sum(0) < len(mask)
+            x = x.clone()[:, ~throw_cols]
+
+        # sort the data
+        sorted_x, _ = torch.sort(x, 0)
+        n = x.shape[0]
+        loss = 0
+        for i in range(x.shape[1]):
+            ecdf_i = (torch.arange(1, n + 1).to(x[:, i].device)) / (n + 1)
+            pcdf_i = self.beta_cdf(sorted_x[:, i]).to(ecdf_i.device)
+            loss += (pcdf_i - ecdf_i).pow(2).sum(0)
+        return loss/n
+
+    def bs_test(self, x: torch.Tensor):
+        pass
+
+    def beta_cdf(self, b: torch.Tensor, npts=100):
+        x = mylinspace(torch.zeros(b.shape[0]).to(b.device)+1e-10, b, npts)
+        return torch.Tensor([torch.trapz(self.distro.log_prob(x[:, i].to('cpu')).exp(), x[:, i].to('cpu')).to(x.device)
+                             for i in range(x.shape[1])])
+
+
+def mylinspace(start: torch.Tensor, stop: torch.Tensor, num: int):
+    """
+    Creates a tensor of shape [num, *start.shape] whose values are evenly spaced from start to end, inclusive.
+    Replicates but the multi-dimensional bahaviour of numpy.linspace in PyTorch.
+    """
+    # create a tensor of 'num' steps from 0 to 1
+    steps = torch.arange(num, dtype=torch.float32, device=start.device) / (num - 1)
+
+    # reshape the 'steps' tensor to [-1, *([1]*start.ndim)] to allow for broadcastings
+    # - using 'steps.reshape([-1, *([1]*start.ndim)])' would be nice here but torchscript
+    #   "cannot statically infer the expected size of a list in this contex", hence the code below
+    for i in range(start.ndim):
+        steps = steps.unsqueeze(-1)
+
+    # the output starts at 'start' and increments until 'stop' in each dimension
+    out = start[None] + steps * (stop - start)[None]
+
+    return out
